2d12acd1e237d2f8e1b2db854a49df7d79b0cd29
Delano Yusuf Habibie <delanoyusufhabibie47@gmail.com>
2023-10-01 15:43:35 +0700

    second commit

--- Smart Trolley/Gui.py
+++ Smart Trolley/Gui.py
@@ -6,6 +6,16 @@ data_barang = {
     '8992761002015': {'nama': 'Coca Cola', 'harga': 6000},
     '8998009010224': {'nama': 'Ultra Milk', 'harga': 7000},
     '8885013130485': {'nama': 'Coffe Crispy', 'harga': 3500},
+    '8886008101053': {'nama': 'Aqua', 'harga': 3000},
+    '8993175539210': {'nama': 'Nextar', 'harga': 2500},
+    '8996001600269': {'nama': 'Lemineral', 'harga': 3500},
+    '9555123709008': {'nama': 'Susu Kedelai Soya', 'harga': 6000},
+    '8992696523081': {'nama': 'Milo Kotak Kecil', 'harga': 3500},
+    '8990044000079': {'nama': 'Pocky', 'harga': 3000},
+    '8996001600504': {'nama': 'Teh Pucuk', 'harga': 4000},
+    '089686010190': {'nama': 'Indomie', 'harga': 3500},
+    '711844130111': {'nama': 'Saos', 'harga': 7000},
+    '8992741958790': {'nama': 'Yupi', 'harga': 4500},
 }
 
 # Data belanjaan dalam bentuk list
@@ -34,14 +44,25 @@ def tambah_ke_tabel(id_barang, kuantitas):
 
         update_tabel()
         hitung_total()
-        
-    else:
-        total_label.config(text="ID Barang tidak valid!")
+
+def hapus_item():
+    selected_item = tree.selection()
+    if selected_item:
+        item_index = int(selected_item[0][1]) - 1
+        if belanjaan[item_index]['kuantitas'] > 1:
+            belanjaan[item_index]['kuantitas'] -= 1
+            belanjaan[item_index]['total'] -= belanjaan[item_index]['harga']
+        else:
+            del belanjaan[item_index]
+        update_tabel()
+        hitung_total()
+
 
 def on_barcode_scan(event):
     id_barang = id_entry.get()
-    tambah_ke_tabel(id_barang, 1)
-    id_entry.delete(0, 'end')  # Menghapus isi input field setelah pemindaian
+    if len(id_barang) == 13:  # Sesuaikan dengan panjang barcode GM66 (contoh: 8992761002015)
+        tambah_ke_tabel(id_barang, 1)
+        id_entry.delete(0, 'end')  # Menghapus isi input field setelah pemindaian
 
 def update_tabel():
     for i in tree.get_children():
@@ -52,9 +73,9 @@ def update_tabel():
 # Membuat jendela utama
 window = tk.Tk()
 window.title('Mart Trolley')
-window.geometry('600x400')
+window.geometry('800x400')
 
-#title
+# Title
 title_label = ttk.Label(master=window, text='Smart Trolley', font='calibri 40')
 title_label.pack()
 
@@ -88,6 +109,10 @@ vsb.pack(side='right', fill='y')
 tree.configure(yscrollcommand=vsb.set)
 
 tree.pack(fill='both', expand=True)
+
+# Tombol untuk menghapus item barang
+hapus_button = ttk.Button(window, text="Hapus", command=hapus_item)
+hapus_button.pack()
 
 # Label untuk menampilkan total keseluruhan harga barang belanjaan
 total_label = tk.Label(window, text="", font='calibri 24')
